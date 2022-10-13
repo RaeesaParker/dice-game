f7a7aba1890c25f4f88389f633ff2930eb7cdd1d
RaeesaParker <raeesaparker101@gmail.com>
2022-10-13 16:00:48 +0100

    Score now added to player 1 holding

--- app.js
+++ app.js
@@ -6,6 +6,20 @@
 let body = document.body;
 
 
+
+// -------- GAME VARIABLES
+
+let playerOne = {
+	holdingScore: 0,
+	totalScore: 0
+}
+
+let playerTwo = {
+	holdingScore: 0,
+	totalScore: 0
+}
+
+
 // ============= Remove overlay screen if any button is detected =============
 
 let secOverscreen = document.getElementById("sec-overscreen");
@@ -24,7 +38,10 @@ let figDie = document.getElementById("fig-die");
 
 // Randomise dice when clicked
 figDie.addEventListener("click", (event) => {
-	let randomNum = Math.floor((Math.random() * 6) + 1)
+	let randomNum = Math.floor((Math.random() * 6) + 1);
+
+	let playerOneHoldingh3 = document.getElementById("play1-hold");
+
 
 	// Change the image depending on which number is selected
 	switch (randomNum) {
@@ -47,4 +64,30 @@ figDie.addEventListener("click", (event) => {
 			figDie.src = "./assets/6.png"
 			break;
 	}
+
+	// Add the random number to player 1's holding 
+
+	playerOne.holdingScore = playerOne.holdingScore + randomNum;
+
+	// Change value of player 1's holding 
+	playerOneHoldingh3.innerText = `${playerOne.holdingScore}`
+
 })
+
+
+
+// ============= Change the value of player 1's hold when die is clicked =============
+
+
+
+
+
+
+
+
+
+//  When the dice is clicked => Change image then change the score. So we need a function 
+
+
+
+
