36e33025620a079113ae5f47a905d042ce8c5371
RaeesaParker <raeesaparker101@gmail.com>
2022-10-14 10:38:48 +0100

    Add resetscores function

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 
+// WRIE A FUNCTION TO RESET EVERYTHING = ADD IT TO THE END SCREEN
+
 
 
 // ======== VARIABLES ============
@@ -21,13 +23,20 @@ let playerTwo = {
 
 let btnNewGame = document.getElementById("btn-newGame");
 
-let secOverscreen = document.getElementById("sec-overscreen");
+let secOverscreen = document.getElementById("start-screen");
 
 let playerOneHoldingh3 = document.getElementById("play1-score-hold");
 
 let playerOneTotalh2 = document.getElementById("play1-score-total");
 
 let playerOneHoldBtn = document.getElementById("play1-btn-hold")
+
+let playerTwoHoldingh3 = document.getElementById("play2-score-hold");
+
+let playerTwoTotalh2 = document.getElementById("play2-score-total");
+
+let playerTwoHoldBtn = document.getElementById("play2-btn-hold")
+
 
 
 
@@ -87,11 +96,13 @@ figDie.addEventListener("click", (event) => {
 })
 
 
+
 // ============= HOLD BUTTON =============
 //  If the hold button is clicked then add the holding score to the total => take holding down to zero
 playerOneHoldBtn.addEventListener("click", () => {
-	playerOne.totalScore = playerOne.holdingScore ; 
+	playerOne.totalScore = playerOne.totalScore + playerOne.holdingScore ; 
 	playerOne.holdingScore = 0;
+
 	updateScoreText()
 })
 
@@ -125,19 +136,38 @@ btnNewGame.addEventListener("click", () => {
 // ============= START GAME =============
 
 let gameStart = () => {
-
 	// Add overscreen to player 2
 	addOverscreen()
 }
 
-
 // Add overscreen to player screen
-
 let addOverscreen = () => {
 	let playerTwoOverscreen = document.getElementById("player2-div")
 	playerTwoOverscreen.style.backgroundColor = "var(--black)"
 }
 
+
+// ============= RESET ALL SCORES ON GAME RESTART =============
+
+
+let resetScores = () => {
+	// Reset scores stored in objects
+	playerOne = {
+		holdingScore: 0,
+		totalScore: 0
+	}
+
+	playerTwo = {
+		holdingScore: 0,
+		totalScore: 0
+	}
+
+	// Reset scores on gameboard
+	playerOneTotalh2.innerText = `0`;
+	playerOneHoldingh3.innerText = `0`
+	playerTwoTotalh2.innerText = `0`;
+	playerTwoHoldingh3.innerText = `0`
+}
 
 
 // ============= DICE OUTCOME =============
@@ -163,8 +193,33 @@ let diceOutcome = (randomNum) => {
 let updateScoreText = () => {
 	playerOneTotalh2.innerText = `${playerOne.totalScore}`;
 	playerOneHoldingh3.innerText = `${playerOne.holdingScore}`
+
+	// If the score is over 20 => Add the ending screen
+	if (playerOne.totalScore > 20){
+		addEndingScreen()
+	}
 }
 
+
+let addEndingScreen = () =>{
+	let endOverscreen = document.getElementById("end-screen")
+	// Turn off the display
+	if (endOverscreen.style.display = "none"){
+		endOverscreen.style.display = "block";
+	}
+
+	body.addEventListener("keydown", (event) => {
+
+	// Turn off the display
+	endOverscreen.style.display = "none"
+
+	// Reset the scores 
+	resetScores();
+
+	// Start Game
+	gameStart();
+	})
+}
 
 
 
